55fc2c6a7d54cb9fff56172408320b40a68e7d7b
rachiddaoudi <rachiddaoudi533@gmail.com>
2023-12-08 15:07:11 +0100

    realiser api login && register

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,14 +3,20 @@ const app = express();
 const dotenv = require("dotenv").config();
 const PORT = 4000;
 const { connect: connectMongo } = require("./frameworks/database/mongo");
-
 const routes = require("./frameworks/expressSpecific/routes");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+// const cookieParser = require("cookie-parser");
 
 module.exports = {
   start: () => {
     app.use(express.json());
+    app.use(cors());
     const apiRoutes = routes();
     app.use("/api/v1", apiRoutes);
+    app.use(bodyParser.json());
+    // app.use(cookieParser());
+
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
       connectMongo();
